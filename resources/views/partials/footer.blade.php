a1ac1f0d495f33cb7abd20cb84106d43f62b58c9
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-16 14:56:11 +0100

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,11 @@
-<footer>
+<footer class="fixed-bottom">
+    <div class="text-center text-light bg-dark">
+        <div class="card-header">
+            Credits
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Titolo non troppo grande</h5>
+            <p class="card-text">Testo indicativo per espimere qualcosa, un po lungo ma non troppo</p>
+        </div>
+    </div>
 </footer>
