941ecced4acff0b10a563283a3921c93a272a743
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-16 16:11:56 +0100

    printed the data of the db

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,7 +2,24 @@
 @section('title', 'Shop')
 
 @section('content')
-<main>
-    <h1>Dress Shop</h1>
+<main class="p-5">
+    <h1 class="text-center">Dress Shop</h1>
+
+
+    <div class="row row-cols-1 row-cols-md-3 p-5">
+        @foreach ($dress as $value)
+        <div class="col mb-4">
+            <div class="card h-100">
+                <div class="card-body bg-light">
+                    <h5 class="card-title text-uppercase">{{ $value->name }}</h5>
+                    <p class="card-text">Tessuto: {{ $value->fabric }} - Colore: {{ $value->color }}</p>
+                    <p class="card-text">Taglia: <span class="text-uppercase">{{ $value->size }}</span></p>
+                    <p class="card-text"><small class="text-dark font-weight-bold">&#x20AC; {{ $value->price }}</small></p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
 </main>
 @endsection
