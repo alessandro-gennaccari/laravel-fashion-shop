3ef2277ef7743c30ff4eb47ff8336f71946707ff
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-03-16 16:10:54 +0100

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,15 @@
 @section('title', 'Homepage')
 
 @section('content')
-<main>
+<main class="p-5 text-center">
     <h1>Home</h1>
+
+    <div class="home-text">
+        <div class="container">
+            <h2 style="font-size: 60px;" class="p-5">Dress Shop</h2>
+            <p style="font-size: 20px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nostrum itaque quod saepe delectus natus porro officiis maxime eligendi similique rem asperiores vel, maiores et error tenetur fugiat. Voluptas, animi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet fugit reiciendis quaerat cupiditate quasi molestias exercitationem porro numquam, hic est animi nam error vero quisquam laboriosam enim commodi possimus.</p>
+            <p style="font-size: 20px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet fugit reiciendis quaerat cupiditate quasi molestias exercitationem porro numquam, hic est animi nam error vero quisquam laboriosam enim commodi possimus.</p>
+        </div>
+    </div>
 </main>
 @endsection
